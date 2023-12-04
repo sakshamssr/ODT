9791e5e0d85b5eb1df8f8e15b4de0b032f87a71a
Saksham Singh <sakshamraghav57@gmail.com>
2023-12-04 10:18:05 +0530

    Update

---
+++ app1/GNews_scrape.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrape():
+    l=[]
+    store={}
+
+    URL = "https://news.google.com/search?q=finance"
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    name = soup.find_all(class_="JtKRv")
+    image=soup.find_all(class_="K0q4G")
+    time=soup.find_all(class_="hvbAAd")
+    link=soup.find_all(class_="WwrzSb")
+
+    for i in range(0,len(name)):
+        try:
+            title=str(name[i]).split('tabindex="0">')[1].split("</h4>")[0]
+            imagelink=str(image[i]).split('src="')[1].split('" src')[0]
+            upload=time[i].text
+            articleLink=str(link[i]).split('href="')[1].split('" jslog')[0]
+            store[i]={"title":title,"image":imagelink,"uploadTime":upload,"articlelink":articleLink}
+        except:
+            print("That's Enough!")
+            break
+    print(store[0])
+    return store
--- app1/views.py
+++ app1/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
 import requests as req
 
+from .GNews_scrape import scrape
+
 url = "https://gnewssapi.vercel.app/news/finance"
 #print(data)
 
 # Create your views here.
 def home(requests):
-    response = req.get(url)
-    data = response.json()
+    response = scrape()
+    data = scrape()
     dataJson={
-        "news1":[{"title":data["0"]["title"],"image":data["0"]["image"],"link":"https://news.google.com/"+data["0"]["articlelink"]},
-                 {"title":data["1"]["title"],"image":data["1"]["image"],"link":"https://news.google.com/"+data["1"]["articlelink"]},
-                 {"title":data["2"]["title"],"image":data["2"]["image"],"link":"https://news.google.com/"+data["2"]["articlelink"]},
-                 {"title":data["3"]["title"],"image":data["3"]["image"],"link":"https://news.google.com/"+data["3"]["articlelink"]},
-                 {"title":data["4"]["title"],"image":data["4"]["image"],"link":"https://news.google.com/"+data["4"]["articlelink"]},
-                 {"title":data["5"]["title"],"image":data["5"]["image"],"link":"https://news.google.com/"+data["5"]["articlelink"]},
-                 {"title":data["6"]["title"],"image":data["6"]["image"],"link":"https://news.google.com/"+data["6"]["articlelink"]},
-                 {"title":data["7"]["title"],"image":data["7"]["image"],"link":"https://news.google.com/"+data["7"]["articlelink"]},]
+        "news1":[{"title":data[0]["title"],"image":data[0]["image"],"link":"https://news.google.com/"+data[0]["articlelink"]},
+                 {"title":data[1]["title"],"image":data[1]["image"],"link":"https://news.google.com/"+data[1]["articlelink"]},
+                 {"title":data[2]["title"],"image":data[2]["image"],"link":"https://news.google.com/"+data[2]["articlelink"]},
+                 {"title":data[3]["title"],"image":data[3]["image"],"link":"https://news.google.com/"+data[3]["articlelink"]},
+                 {"title":data[4]["title"],"image":data[4]["image"],"link":"https://news.google.com/"+data[4]["articlelink"]},
+                 {"title":data[5]["title"],"image":data[5]["image"],"link":"https://news.google.com/"+data[5]["articlelink"]},
+                 {"title":data[6]["title"],"image":data[6]["image"],"link":"https://news.google.com/"+data[6]["articlelink"]},
+                 {"title":data[7]["title"],"image":data[7]["image"],"link":"https://news.google.com/"+data[7]["articlelink"]},]
     }
     #print(dataJson["news1"])
     return render(requests,"home.html",dataJson)
