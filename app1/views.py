f3ee70c1f3bdf6ec098753bfa4e7a7eb6d1e9ed8
Saksham Singh <sakshamraghav57@gmail.com>
2024-03-21 10:54:50 +0530

    Update

--- app1/views.py
+++ app1/views.py
@@ -64,9 +64,58 @@ def search(request,para):
     return render(request,"searchpage.html",context)
 
 def details(request,para,link,para3,bondname):
+    from bs4 import BeautifulSoup
+    store={}
+
+    URL="https://markets.businessinsider.com/bonds/"+link
+    headers={"User-Agent": "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"}
+
+    page = req.get(URL,headers=headers)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    table=soup.find_all(class_="table__tr")
+    scripts=soup.find_all("script")
+
+    st=[]
+    rating=soup.find(class_="moodys-rating__rating")
+
+
+    for i in range(0,len(scripts)):
+        st.append(str(scripts[i]))
+        
+    tabledata=[]
+
+    for i in table:
+        if('class="table__td">' in str(i)):
+            tabledata.append(str(i))
+
+    for i in range(0,len(tabledata)-5):
+        base=str(tabledata[i]).replace("\t","").replace("\n",'')
+        name=base.split('class="table__td">')[1].split('</td>')[0]
+        data=base.split('class="table__td">')[1].split('</td>')[1].split('<td class="table__td text-right">')[1]
+
+        store[name]=data
+
+    Issue=str(store["Issuer"]).split('">')[1].split("</a>")[0]
+    store["Issuer"]=Issue
+    
+    try:
+        rating=rating.text
+        store["rating"]=100-int(rating)
+        if (int(rating)<33):
+            store["color"]="green"
+        elif(int(rating)>33 and int(rating)<66):
+            store["color"]="orange"
+        else:
+            store["color"]="red"
+    except:
+        store["rating"]="-"
+    store["graphdata"]=st[23].replace("<script>","").replace("</script>","").replace("=","").replace(" ","").replace("null","None").replace("false","False").replace("true","True").replace("\n","").replace("\t","").replace('/','')
+
+    store["graphdata"]=store["graphdata"].split(";")[0].replace("vardetailChartViewmodel","")
     from json import dumps
     # response=req.get("https://bonds-terminal.vercel.app/search2/details/"+str(link))
-    data=businessi(str(link))
+    data=store
     print(data)
 
     fetchgraphData=eval(data["graphdata"])
